2a6309dfdf299713b8e8a8d246227489708bdb22
abreefpilz <abreefpilz@vt.edu>
2021-11-18 14:22:15 -0500

    Added EXO data to WQ file for CCR

---
+++ Add_EXO_to_Gateway.R
@@ -0,0 +1,25 @@
+#Combin the manual EXO data into the streamed file and push to Annie
+
+pacman::p_load("tidyverse", "lubridate","rqdatatable")
+
+#Get up to date collated EXO file
+download.file("https://raw.githubusercontent.com/CareyLabVT/ManualDownloadsSCCData/master/CCR_manual_downloads/CCR_1_5_EXO_downloads/Collated_CCR_1_5_EXO_.csv","CCR_EXO.csv")
+
+ccrwaterdata <- read_csv("ccre-waterquality.csv", col_names = T, 
+                          col_types = cols(.default = col_double(), TIMESTAMP = col_character()), show_col_types = T)
+EXO= read_csv("CCR_EXO.csv", col_names=T,
+              col_types = cols(.default = col_double(), TIMESTAMP = col_character()), show_col_types = T)
+#Take out the columns that don't match
+EXO=EXO%>%
+  select(-c(nLF.Cond.uS.cm, Vertical.Position.m))
+
+#Join the CCR-waterquality data and the EXO data
+CCR<- natural_join(ccrwaterdata, EXO, 
+                          by = "TIMESTAMP",
+                          jointype = "LEFT")
+wQnames=colnames(ccrwaterdata)
+#rearrange 
+CCR=CCR%>%
+  select(wQnames)
+
+write.csv(CCR, "ccre-waterquality.csv",na="NAN", row.names = FALSE)
